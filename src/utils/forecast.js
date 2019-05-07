1d7d327a9edf4fc4d9b802d182cdf59ee5a8906e
Eric Wang <ericbwang94@gmail.com>
2019-05-06 18:19:38 -0700

    Reworded weather message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain. The high will be ' + body.daily.data[0].temperatureHigh + 'degrees, and the low will be ' + body.daily.data[0].temperatureLow + ' degrees.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. The high today is ' + body.daily.data[0].temperatureHigh + ' degrees, with a low of ' + body.daily.data[0].temperatureLow + ' degrees. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
